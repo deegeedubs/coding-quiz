e20514f7c1c0dfd0b3881c7df9b8c8e7978bed1a
Danny Wittig <wittigdaniel.g@gmail.com>
2023-04-26 23:05:30 -0500

    added updating score
    made sure score is updating properly, you can now save your score.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var questionCurrent;
-var questionContainer = document.querySelector("body");
+var buttonBox = document.querySelector(".buttons");
+var questionContainer = document.querySelector(".question-container");
 var questionText = document.querySelector("h2");
 var answerBox = document.querySelector(".choice-box"); //ul
 var list = answerBox.querySelectorAll(".noButton"); //li
@@ -9,8 +10,20 @@ var messageIntro = message.textContent;
 var score = document.querySelector("#score");
 var timer = document.querySelector("#time");
 var playButton = document.querySelector(".play-button");
+var lastScore = document.querySelector("#last-score");
+
+var finalScore = document.createElement("h3");
+questionContainer.appendChild(finalScore);
 
 var reset = document.createElement("button");
+buttonBox.appendChild(reset);
+reset.classList.add("play-button");
+reset.textContent = "Restart";
+
+var saveScore = document.createElement("button");
+buttonBox.appendChild(saveScore);
+saveScore.classList.add("play-button");
+saveScore.textContent = "Save Score";
 
 var questions = [
   "What is an API?",
@@ -26,26 +39,16 @@ var wrong1s = ["Application Page Input", "function{};", ".appendChild"];
 
 var scoreValue = 0;
 
+lastScore.textContent = localStorage.getItem("highscore");
+
+resetGame();
+
+//! Event Listeners 
+
 // Click start button to begin the quiz -- event listener to call game function
 playButton.addEventListener("click", function () {
-  playButton.style.display = "none";
-
-  // questionContainer.appendChild(reset);
-  // reset.classList.add("play-button");
-  // reset.textContent = "Restart";
   gameStart();
 });
-
-// function to begin the game
-function gameStart() {
-  answerBox.setAttribute("style", "display: flex");
-  // array of questions and answers
-  message.textContent = messageIntro;
-  score.textContent = "Score: " + scoreValue + " points";
-  timer.textContent = "Time: 30 Seconds Remaining";
-  callQuestion(i);
-  startTimer();
-}
 
 // if the correct answer is selected: display message, increase score and move to next question in list
 choices[0].addEventListener("click", function (event) {
@@ -55,8 +58,9 @@ choices[0].addEventListener("click", function (event) {
   }, 1000);
   i++;
   callQuestion(i);
-  scoreValue = scoreValue + 5;
-  console.log(scoreValue);
+  if (i<questions.length){
+    scoreValue = scoreValue + 5;
+  }
   score.textContent = "Score: " + scoreValue + " points";
 });
 
@@ -66,7 +70,30 @@ choices[1].addEventListener("click", function (event) {
   setTimeout(function () {
     message.textContent = messageIntro;
   }, 1000);
+  scoreValue = scoreValue - 1;
+  timeLeft = timeLeft - 5;
+  score.textContent = "Score: " + scoreValue + " points";
 });
+
+reset.addEventListener("click", resetGame);
+
+saveScore.addEventListener("click", function(){
+    var initials = window.prompt("Enter your initials:");
+    lastScore.textContent = initials + " " + scoreValue;
+    localStorage.setItem("lastscore", lastScore.textContent);
+})
+
+//! Functions
+
+// function to begin the game
+function gameStart() {
+    answerBox.setAttribute("style", "display: flex");
+    playButton.style.display = "none";
+    reset.style.display = "block";
+    // array of questions and answers
+    callQuestion(i);
+    startTimer();
+  }
 
 var i = 0;
 // Store next question into object
@@ -81,25 +108,48 @@ function callQuestion(i) {
   choices[1].textContent = questionCurrent.wrong1;
 
   if (i === questions.length) {
-    questionText.textContent = "Game Over.";
-    message.textContent = messageIntro;
-    answerBox.style.display = "none";
+    gameOver();
   }
 }
-
+var timerInterval;
 var timeLeft = 30;
 function startTimer() {
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     timeLeft--;
     timer.textContent = "Time: " + timeLeft + " Seconds Remaining";
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      questionText.textContent = "Game Over.";
-      message.textContent = messageIntro;
-      answerBox.style.display = "none";
+    if (timeLeft < 1) {
+      gameOver();
     }
     if (i === questions.length) {
       clearInterval(timerInterval);
     }
   }, 1000);
+}
+
+function gameOver() {
+  clearInterval(timerInterval);
+  questionText.textContent = "Game Over.";
+  message.textContent = messageIntro;
+  answerBox.style.display = "none";
+  scoreValue = scoreValue + timeLeft;
+  finalScore.style.display = "block";
+  finalScore.textContent = "Your final score is: " + scoreValue + " points.";
+
+  saveScore.style.display = "block";
+}
+
+function resetGame() {
+  scoreValue = 0;
+  timeLeft = 30;
+  i = 0;
+  clearInterval(timerInterval);
+  message.textContent = messageIntro;
+  score.textContent = "Score: " + scoreValue + " points";
+  timer.textContent = "Time: 30 Seconds Remaining";
+  questionText.textContent = "Press Start To Begin The Quiz";
+  reset.style.display = "none";
+  playButton.style.display = "block";
+  answerBox.setAttribute("style", "display: none");
+  finalScore.style.display = "none";
+  saveScore.style.display = "none";
 }
